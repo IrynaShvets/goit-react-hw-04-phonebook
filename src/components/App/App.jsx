3156ac51b9c9b55b700dc0a9d6a027318fb3bdc7
IrynaShvets <irinashwets@ukr.net>
2022-03-08 14:03:17 +0200

    хуки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { nanoid } from 'nanoid';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,7 +8,78 @@ import Filter from '../Filter';
 import Container from '../Container';
 import { TitleMain, Title } from './App.styled';
 
-class App extends Component {
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+  return [state, setState];
+};
+
+function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  // --- додаю контакти до списку ---
+  const addContact = ({ name, number }) => {
+    const contact = { id: nanoid(), name, number };
+
+    const normalizedName = contact.name.toLowerCase();
+    const isNameInContacts = contacts.some(contact =>
+      contact.name.toLowerCase().includes(normalizedName),
+    );
+
+    if (isNameInContacts) {
+      return toast(`${name} is already in contacts.`);
+    }
+
+    setContacts(contacts => [contact, ...contacts]);
+  };
+
+  //--- видаляю контакт зі списку ---
+  const deleteContact = contactId => {
+    setContacts(contacts =>
+      contacts.filter(contact => contact.id !== contactId),
+    );
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  // --- пошук по фільтру ---
+  const getVisibleContacts = useMemo(() => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter),
+    );
+  }, [filter, contacts]);
+
+  return (
+    <Container>
+      <ToastContainer />
+      <TitleMain>Phonebook</TitleMain>
+      <ContactForm onSubmit={addContact} />
+      <Title>Contacts</Title>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList
+        contacts={getVisibleContacts}
+        onDeleteContact={deleteContact}
+      />
+    </Container>
+  );
+}
+
+export default App;
+
+//-------class-------
+/* class App extends Component {
   state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -104,3 +175,4 @@ class App extends Component {
 }
 
 export default App;
+ */
